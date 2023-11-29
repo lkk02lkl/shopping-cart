c4ede3e77bcc8b7508abdffb3396199301178db7
unknown <anhelinapavliuk@gmail.com>
2023-11-29 17:24:08 +0100

    add

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+let shopItemsData = [
+  {
+    id: "3325488",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-1.jpg"
+  }, {
+    id: "320525669",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-2.jpg"
+  }, {
+    id: "327658580",
+    name: "T-Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-3.jpg"
+  }, {
+    id: "32268528515",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-4.jpg"
+  }
+];
